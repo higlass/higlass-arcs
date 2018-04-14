2f6d7ad7b3276e64a9cc45603308ee08bbd89ed2
Fritz Lekschas <service@lekschas.de>
2018-04-14 17:35:31 -0400

    Move config to track file for simplification

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import register from 'higlass-register';
 
 import GeoJsonTrack from './GeoJsonTrack';
-import config from './config';
 
 register({
   name: 'GeoJsonTrack',
   track: GeoJsonTrack,
-  config,
+  config: GeoJsonTrack.config,
 });

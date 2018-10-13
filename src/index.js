640b5323581295bf87f9c33fed5051ccd4c4e364
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-10-13 18:36:06 -0400

    Remove console log statements

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import register from 'higlass-register';
 
 import Arcs1DTrack from './Arcs1DTrack';
 
-console.log('config:', Arcs1DTrack.config);
-
 register({
   name: 'Arcs1DTrack',
   track: Arcs1DTrack,
   config: Arcs1DTrack.config,
 });
-
-console.log('window.higlassTracks', window.higlassTracks);
